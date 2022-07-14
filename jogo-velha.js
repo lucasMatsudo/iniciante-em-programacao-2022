56496ec0dbd5ecff1ee61830998747e09c83fac2
lucasMatsudo <109229677+lucasMatsudo@users.noreply.github.com>
2022-07-13 21:41:05 -0300

    Add files via upload

---
+++ jogo-velha.js
@@ -0,0 +1,57 @@
+var jogador = null;
+var jogadorAtivo = document.getElementById('jogador-ativo');
+const TodosOsQuadradosJogo = document.querySelectorAll(".quadrado-jogo");
+const X = "X";
+const O = "O";
+
+mudaJogador('X');
+
+quadrado.innerHTML = gamer;
+    square.style.color = '#000';
+
+    if (jogador === 'X') {
+        jogador = 'O';
+    } else {
+        jogador = 'X';
+    }
+
+    mudaJogador(gamer);
+
+function mudaJogador(valor) {
+    jogador = valor;
+    jogadorAtivo.innerHTML = jogador;
+}
+
+function verificaVitoriaDoJogador(){
+
+    const a1 = document.querySelector(`[data-linha='${1}'][data-coluna='${1}']`);
+    const a2 = document.querySelector(`[data-linha='${1}'][data-coluna='${2}']`);
+    const a3 = document.querySelector(`[data-linha='${1}'][data-coluna='${3}']`);
+
+    const b1 = document.querySelector(`[data-linha='${2}'][data-coluna='${1}']`);
+    const b2 = document.querySelector(`[data-linha='${2}'][data-coluna='${2}']`);
+    const b3 = document.querySelector(`[data-linha='${2}'][data-coluna='${3}']`);
+
+    const c1 = document.querySelector(`[data-linha='${3}'][data-coluna='${1}']`);
+    const c2 = document.querySelector(`[data-linha='${3}'][data-coluna='${2}']`);
+    const c3 = document.querySelector(`[data-linha='${3}'][data-coluna='${3}']`);
+
+    let condicoesDeVitoria= [
+        [a1, a2, a3]
+        [a1, b1, c1]
+        [a1, b3, c3]
+        [b1, b2, b3]
+        [c1, c2, c3]
+        [c1, b2, a3]
+        [a2, b2, c2]
+        [a3, b3, c3]
+    ]
+    let posicoesQuadrados = [a1, a2, a3, b1, b2, b3, c1, c2, c3]
+
+/*
+function reiniciarJogo() {
+    window.location.reload();
+    }
+*/
+
+}
